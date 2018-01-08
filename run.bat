0f7f55306a978ff43a56180c0045e49b0780a711
Reto Weiss <reto.weiss@ivyteam.ch>
2018-01-08 19:49:35 +0100

    * Do not copy dependency to lib folder instead use maven to run manual tests

--- run.bat
+++ run.bat
@@ -1,1 +1,5 @@
-"c:\program files\java\jdk-9.0.1\bin\java" -cp lib/mvn/*;target/classes;target/test-classes ch.weiss.jmx.client.cli.JmxClientCli %*
+SET JAVA_HOME=c:\program files\java\jdk-9.0.1
+SET MAVEN=C:\Tools\maven\apache-maven-3.5.2\bin\mvn
+SET MAIN_CLASS=ch.weiss.jmx.client.cli.JmxClientCli
+
+%MAVEN% exec:java -Dexec.mainClass=%MAIN_CLASS% -Dexec.args="%*" -Dexec.classpathScope=test
