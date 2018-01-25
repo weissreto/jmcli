0d820a12a0fd342e8ab6cb64010ffd84b7f34950
Reto Weiss <reto.weiss@ivyteam.ch>
2018-01-25 21:57:24 +0100

    * DeltaDataChannel can also handle int, float and double

--- src/main/java/ch/rweiss/jmx/client/cli/chart/data/channel/DeltaDataChannel.java
+++ src/main/java/ch/rweiss/jmx/client/cli/chart/data/channel/DeltaDataChannel.java
@@ -2,7 +2,7 @@ package ch.rweiss.jmx.client.cli.chart.data.channel;
 
 public class DeltaDataChannel extends FunctionDataChannel
 {
-  public Long lastValue;
+  public Object lastValue;
   public DeltaDataChannel(DataChannel baseValueDataChannel)
   {
     super(baseValueDataChannel.name(), baseValueDataChannel);
@@ -11,13 +11,38 @@ public class DeltaDataChannel extends FunctionDataChannel
   @Override
   protected Object apply(Object value)
   {
-    Long currentValue = (Long)value;
-    long delta = 0L;
-    if (lastValue != null && currentValue != null)
+    Object delta;
+    lastValue = getLastOrInitValue(value);
+    if (value instanceof Long)
     {
-      delta = currentValue-lastValue;
+      delta = (Long)value-(Long)lastValue;
     }
-    lastValue = currentValue;
+    else if (value instanceof Integer)
+    {
+      delta = (Integer)value-(Integer)lastValue;
+    }
+    else if (value instanceof Float)
+    {
+      delta = (Float)value-(Float)lastValue;
+    }
+    else if (value instanceof Double)
+    {
+      delta = (Double)value-(Double)lastValue;
+    }
+    else
+    {
+      delta = value;
+    }
+    lastValue = value;
     return delta;
+  }
+
+  private Object getLastOrInitValue(Object value)
+  {
+    if (lastValue != null)
+    {
+      return lastValue;
+    }
+    return value;
   }
 }
