52c96255dd6d624d1a2e81d025e8ce3b18e4a0b1
Reto Weiss <reto.weiss@ivyteam.ch>
2018-09-29 20:34:48 +0200

    * Respect environment variable JAVA_HOME if it is set under windows

--- src/assembly/windows/bin/jmcli.bat
+++ src/assembly/windows/bin/jmcli.bat
@@ -5,4 +5,13 @@ SET PROGRAM_HOME_DIR=%PROGRAM_BIN_DIR%..
 SET MAIN_CLASS=ch.rweiss.jmx.client.cli.JmxClientCli
 SET CLASS_PATH=%PROGRAM_HOME_DIR%\lib\*
 
-java.exe -cp %CLASS_PATH% -Djava.library.path=%PROGRAM_BIN_DIR% %MAIN_CLASS% %*
+IF NOT DEFINED JAVA_HOME 
+(
+  SET JAVA_EXECUTABLE=java.exe
+) 
+ELSE
+(
+  SET JAVA_EXECUTABLE=%JAVA_HOME%\bin\java.exe
+)
+
+%JAVA_EXECUTABLE% -cp %CLASS_PATH% -Djava.library.path=%PROGRAM_BIN_DIR% %MAIN_CLASS% %*
