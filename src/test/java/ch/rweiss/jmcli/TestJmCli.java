6170e044f55c410dd2c71394b788329f26cfa458
Reto Weiss <reto.weiss@ivyteam.ch>
2019-01-02 17:06:56 +0100

    * Fix new line for linux in test fixtures

--- src/test/java/ch/rweiss/jmcli/TestJmCli.java
+++ src/test/java/ch/rweiss/jmcli/TestJmCli.java
@@ -5,36 +5,38 @@ import org.junit.jupiter.api.extension.RegisterExtension;
 
 public class TestJmCli
 {
+  private static final String NEW_LINE = System.lineSeparator();
+  
   private static final String STANDARD_COMMAND_OUTPUT = "\n"+
-  "Java Virtual Maschines\n"+
-  "\n"+
-  "Id        Display Name\n";
+      "Java Virtual Maschines\n"+
+      "\n"+
+      "Id        Display Name\n";
 
   private static final String VERSION_TEXT = 
-      "Java Management Command Line Interface\r\n" + 
-      "0.3\r\n" + 
-      "(c) 2018 Reto Weiss\r\n";
+      "Java Management Command Line Interface" + NEW_LINE + 
+      "0.3"  + NEW_LINE + 
+      "(c) 2018 Reto Weiss" + NEW_LINE;
 
   private static final String HELP_TEXT = 
-      "\033[33m       _   __  __    _____   _   _ \033[39m\033[0m\r\n"+
-      "\033[33m      | | |  \\/  |  / ____| | | (_)\033[39m\033[0m\r\n"+
-      "\033[33m      | | | \\  / | | |      | |  _ \033[39m\033[0m\r\n"+
-      "\033[33m  _   | | | |\\/| | | |      | | | |\033[39m\033[0m\r\n"+
-      "\033[33m | |__| | | |  | | | |____  | | | |\033[39m\033[0m\r\n"+
-      "\033[33m  \\____/  |_|  |_|  \\_____| |_| |_|\033[39m\033[0m\r\n"+
-      "\r\n"+
-       "Usage: \033[1mjmcli\033[21m\033[0m [\033[33m-hvV\033[39m\033[0m] [COMMAND]\r\n"+
-       "Java Management Command Line Interface\r\n"+
-       "  \033[33m-h\033[39m\033[0m, \033[33m--help\033[39m\033[0m      Display this help and exit\r\n"+
-       "  \033[33m-v\033[39m\033[0m, \033[33m--verbose\033[39m\033[0m   Display details message\r\n"+
-       "  \033[33m-V\033[39m\033[0m, \033[33m--version\033[39m\033[0m   Display version info and exit\r\n"+
-       "Commands:\r\n"+
-       "  \033[1mlist\033[21m\033[0m       Lists objects\r\n"+
-       "  \033[1minfo\033[21m\033[0m       Prints information about an object\r\n"+
-       "  \033[1mset\033[21m\033[0m        Sets values\r\n"+
-       "  \033[1minvoke\033[21m\033[0m     Invokes operation\r\n"+
-       "  \033[1mchart\033[21m\033[0m      Draws a chart\r\n"+
-       "  \033[1mdashboard\033[21m\033[0m  Draws a dashboard\r\n";
+      "\033[33m       _   __  __    _____   _   _ \033[39m\033[0m" + NEW_LINE +
+      "\033[33m      | | |  \\/  |  / ____| | | (_)\033[39m\033[0m" + NEW_LINE +
+      "\033[33m      | | | \\  / | | |      | |  _ \033[39m\033[0m" + NEW_LINE +
+      "\033[33m  _   | | | |\\/| | | |      | | | |\033[39m\033[0m" + NEW_LINE +
+      "\033[33m | |__| | | |  | | | |____  | | | |\033[39m\033[0m" + NEW_LINE +
+      "\033[33m  \\____/  |_|  |_|  \\_____| |_| |_|\033[39m\033[0m" + NEW_LINE +
+      "" + NEW_LINE +
+       "Usage: \033[1mjmcli\033[21m\033[0m [\033[33m-hvV\033[39m\033[0m] [COMMAND]" + NEW_LINE +
+       "Java Management Command Line Interface" + NEW_LINE +
+       "  \033[33m-h\033[39m\033[0m, \033[33m--help\033[39m\033[0m      Display this help and exit" + NEW_LINE +
+       "  \033[33m-v\033[39m\033[0m, \033[33m--verbose\033[39m\033[0m   Display details message" + NEW_LINE +
+       "  \033[33m-V\033[39m\033[0m, \033[33m--version\033[39m\033[0m   Display version info and exit" + NEW_LINE + 
+       "Commands:" + NEW_LINE +
+       "  \033[1mlist\033[21m\033[0m       Lists objects" + NEW_LINE +
+       "  \033[1minfo\033[21m\033[0m       Prints information about an object" + NEW_LINE +
+       "  \033[1mset\033[21m\033[0m        Sets values" + NEW_LINE +
+       "  \033[1minvoke\033[21m\033[0m     Invokes operation" + NEW_LINE +
+       "  \033[1mchart\033[21m\033[0m      Draws a chart" + NEW_LINE +
+       "  \033[1mdashboard\033[21m\033[0m  Draws a dashboard" + NEW_LINE;
   
   @RegisterExtension
   public CommandTester tester = new CommandTester();
@@ -82,7 +84,7 @@ public class TestJmCli
   {
     JmCli.main(new String[] {"--wrongOption"});
     tester.assertStdErr().isEqualTo(
-        "Unknown option: --wrongOption\r\n"+
+        "Unknown option: --wrongOption" + NEW_LINE+
         HELP_TEXT);
   }
   
